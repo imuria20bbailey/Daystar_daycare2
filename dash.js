6c84d711416b3c3501ad16596169e113356a536f
imuria20bbailey <barbraoluka20@gmail.com>
2024-03-27 23:22:17 +0300

    all changes

--- dash.js
+++ dash.js
@@ -16,3 +16,52 @@ function validate() {
         }
     }
 }
+
+//sitter management
+$(document).ready(function() {
+     
+    // INPUT KEY
+    $('input.numbers').keyup(function(event) {
+        if(event.which >= 37 && event.which <= 40) return;
+        $(this).val(function(index, value) {
+            return value
+            .replace(/\D/g, "")
+            .replace(/\B(?=(\d{3})+(?!\d))/g, "");
+        });
+    });
+
+
+
+    document.getElementById("register-sitter-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    // Retrieve form data
+    var formData = {
+        name: document.getElementById("name").value,
+        location: document.getElementById("location").value,
+        dob: document.getElementById("date-of_birth").value,
+        nextOfKin: document.getElementById("next_of_kin").value,
+        nin: document.getElementById("nin").value,
+        recommenderName: document.getElementById("name_of_recommender").value,
+        religion: document.getElementById("religion").value,
+        educationLevel: document.getElementById("level_of_education").value,
+        phoneNumber: document.getElementById("phoneNumber").value
+    };
+
+    // Send form data to backend
+    // var xhr = new XMLHttpRequest();
+    // xhr.open("POST", "/register-sitter", true); // Assuming your registration endpoint is '/users/register'
+    // xhr.setRequestHeader("Content-Type", "application/json");
+    // xhr.onreadystatechange = function() {
+    //     if (xhr.readyState === 4) {
+    //         if (xhr.status === 201) {
+    //             alert("User registered successfully");
+    //             // Clear form fields or do any additional actions
+    //         } else {
+    //             alert("Error: " + xhr.statusText);
+    //         }
+    //     }
+    // };
+    // xhr.send(JSON.stringify(formData));
+});
+});
